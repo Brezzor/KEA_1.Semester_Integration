e029e379e7f6985b76bf4d6e1a010dd00bcbdb8a
Brezzor <oliver.bresson@gmail.com>
2025-02-12 19:47:08 +0100

    opgave '00' node.js data format server

---
+++ 00._Opgaver/02._Data_Format_Server/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import { FileReaderMethods } from "./fileReaderMethods.js";
+import { Me } from "./me.js";
+
+const PORT = 8080;
+
+const app = express();
+
+app.get("/json", async (req, res) => {
+  const me = await FileReaderMethods.readJsonFile();
+  res.status(200).send({ me });
+});
+
+app.get("/xml", async (req, res) => {
+  const me = await FileReaderMethods.readXmlFile();
+  res.status(200).send({ me });
+});
+
+app.get("/yaml", async (req, res) => {
+  const me = await FileReaderMethods.readYamlFile();
+  res.status(200).send({ me });
+});
+
+app.get("/csv", async (req, res) => {
+  const me = await FileReaderMethods.readCsvFile();
+  res.status(200).send({ me });
+});
+
+app.get("/txt", async (req, res) => {
+  const me = await FileReaderMethods.readTxtFile();
+  res.status(200).send({ me });
+});
+
+app.listen(PORT, () => console.log(`Link to server: Http://localhost:${PORT}`));
